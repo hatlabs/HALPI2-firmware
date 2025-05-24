e2d2c58425dd3c517b2ed3063637fd6f4a05e529
Matti Airas <matti.airas@hatlabs.fi>
2025-05-24 16:59:29 +0300

    Change the generated package name

--- gen-bindings/src/main.rs
+++ gen-bindings/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     python::build_package(
         std::env::current_dir().unwrap().as_path(),
         PackageInfo {
-            name: "py_halpi_firmware".into(),
+            name: "halpi2-fw-i2c-postcard".into(),
             version: "0.1.0".try_into().unwrap(),
         },
         python::GenerationSettings::enable_all(),
